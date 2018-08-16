a892b62d503a14e828c3bde9bea9851d628130c8
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-08-16 23:56:54 +0430

    preprocessing _ wtih gender column
    one hot encoding encoding gender

---
+++ Part 3 - Classification/Section 8 - Logistic Regression/lr_with_gender.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 16 23:46:04 2018
+
+@author: Mohammad Doosti Lakhani
+"""
+
+# imporing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+x = dataset.iloc[:,1:4].values
+y = dataset.iloc[:,-1].values
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+standardscaler_x = StandardScaler()
+standardscaler_x = standardscaler_x.fit(x[:,1:3])
+x[:,1:3] = standardscaler_x.transform(x[:,1:3])
+
+# encoding categorial data (Gender) types to labelEncoder and onehotencoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+labelencoder_x = labelencoder_x.fit(x[:,0])
+x[:,0] = labelencoder_x.transform(x[:,0])
+
+onehotencoder_x = OneHotEncoder(categorical_features=[0])
+onehotencoder_x = onehotencoder_x.fit(x)
+x = onehotencoder_x.transform(x).toarray()
+
+# splitting dataset into Train set and Test set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y, train_size = 0.75 , random_state=0)
+
