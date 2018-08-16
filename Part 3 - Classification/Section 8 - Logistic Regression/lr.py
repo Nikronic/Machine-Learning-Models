0b81fe3dd1093f9de17f26ecbe2977d2821a8296
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-08-17 02:21:20 +0430

    Fitting and predicting and accuracy

--- Part 3 - Classification/Section 8 - Logistic Regression/lr.py
+++ Part 3 - Classification/Section 8 - Logistic Regression/lr.py
@@ -8,23 +8,53 @@ Created on Thu Aug 16 23:14:59 2018
 """############################## based on assignment, we do not include Gender  ########################"""
 
 
-# imporing libraries
+# Imporing libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# importing dataset
+# Importing dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 x = dataset.iloc[:,2:4].values
 y = dataset.iloc[:,4].values
 
-# feature scaling
+# Feature scaling
 from sklearn.preprocessing import StandardScaler
 standardscaler_x = StandardScaler()
 standardscaler_x = standardscaler_x.fit(x)
 x = standardscaler_x.transform(x)
 
-# splitting dataset into Train set and Test set
+# Splitting dataset into Train set and Test set
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y, train_size = 0.75 , random_state=0)
 
+# Fitting Logistic Regression model to train data
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0, solver='liblinear')
+classifier = classifier.fit(x_train,y_train)
+
+# Make the prediction on train set
+y_train_pred = classifier.predict(x_train)
+
+# Make the prediction on train set
+y_test_pred = classifier.predict(x_test)
+
+# Acurracy on test and train set
+from sklearn.metrics import confusion_matrix
+cm_train = confusion_matrix(y_train,y_train_pred)
+cm_test = confusion_matrix(y_test,y_test_pred)
+
+# Function for acurracy
+def acc(confusion_matrix):
+    t = confusion_matrix[0][0] + confusion_matrix[1][1]
+    f = confusion_matrix[0][1] + confusion_matrix[1][0]
+    ac = t/(t+f)
+    return (t,f,ac)
+
+t_train,f_train,acc_train = acc(cm_train)
+print('Train status = #{} True, #{} False, %{} Accuracy'.format(t_train,f_train,acc_train*100))
+
+t_test,f_test,acc_test = acc(cm_test)
+print('Test status = #{} True, #{} False, %{} Accuracy'.format(t_test,f_test,acc_test*100))
+    
+    
