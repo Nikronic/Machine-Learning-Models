847596c068936b3c1aa0be9293f0024396dde865
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-08-17 23:34:44 +0430

    typo

--- Part 3 - Classification/Section 9 - K-Nearest Neighbors (K-NN)/knn.py
+++ Part 3 - Classification/Section 9 - K-Nearest Neighbors (K-NN)/knn.py
@@ -86,7 +86,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('K-Nearest Neighbors (Training set)')
+plt.title('K-Nearest Neighbors (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
