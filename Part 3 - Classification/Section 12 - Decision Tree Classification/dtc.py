742a3266a02b907314013442007ee276bc524e3b
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-09-08 21:31:28 +0430

    feature scaling removed
    just use for visualizing. (step goes bigger so we do not need any scaling!)

--- Part 3 - Classification/Section 12 - Decision Tree Classification/dtc.py
+++ Part 3 - Classification/Section 12 - Decision Tree Classification/dtc.py
@@ -16,12 +16,6 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 x = dataset.iloc[:,2:4].values
 y = dataset.iloc[:,4].values
 
-# Feature scaling
-from sklearn.preprocessing import StandardScaler
-standardscaler_x = StandardScaler()
-standardscaler_x = standardscaler_x.fit(x)
-x = standardscaler_x.transform(x)
-
 # Splitting dataset into Train set and Test set
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y, train_size = 0.75 , random_state=0)
@@ -65,8 +59,8 @@ print('Test status = #{} True, #{} False, %{} Accuracy'.format(t_test,f_test,acc
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = x_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 1),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 1))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
@@ -83,8 +77,8 @@ plt.show()
 # Visualising the Test set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = x_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 1),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 1))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
