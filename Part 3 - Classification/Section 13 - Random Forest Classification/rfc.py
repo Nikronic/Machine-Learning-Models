7de3e43715ee41d946d74c5d26c76b62f493250c
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-09-09 14:49:22 +0430

    adding CAP analysis

--- Part 3 - Classification/Section 13 - Random Forest Classification/rfc.py
+++ Part 3 - Classification/Section 13 - Random Forest Classification/rfc.py
@@ -56,7 +56,60 @@ print('Train status = #{} True, #{} False, %{} Accuracy'.format(t_train,f_train,
 t_test,f_test,acc_test = acc(cm_test)
 print('Test status = #{} True, #{} False, %{} Accuracy'.format(t_test,f_test,acc_test*100))
       
+#### CAP Analysis#####
+from scipy import integrate
+def capcurve(y_values, y_preds_proba,title):
+    num_pos_obs = np.sum(y_values)
+    num_count = len(y_values)
+    rate_pos_obs = float(num_pos_obs) / float(num_count)
+    ideal = pd.DataFrame({'x':[0,rate_pos_obs,1],'y':[0,1,1]})
+    xx = np.arange(num_count) / float(num_count - 1)
+    
+    y_cap = np.c_[y_values,y_preds_proba]
+    y_cap_df_s = pd.DataFrame(data=y_cap)
+    y_cap_df_s = y_cap_df_s.sort_values([1], ascending=False).reset_index(level = y_cap_df_s.index.names, drop=True)
+    
+    print(y_cap_df_s.head(20))
+    
+    yy = np.cumsum(y_cap_df_s[0]) / float(num_pos_obs)
+    yy = np.append([0], yy[0:num_count-1]) #add the first curve point (0,0) : for xx=0 we have yy=0
+    
+    percent = 0.5
+    row_index = int(np.trunc(num_count * percent))
+    
+    val_y1 = yy[row_index]
+    val_y2 = yy[row_index+1]
+    if val_y1 == val_y2:
+        val = val_y1*1.0
+    else:
+        val_x1 = xx[row_index]
+        val_x2 = xx[row_index+1]
+        val = val_y1 + ((val_x2 - percent)/(val_x2 - val_x1))*(val_y2 - val_y1)
+    
+    sigma_ideal = 1 * xx[num_pos_obs - 1 ] / 2 + (xx[num_count - 1] - xx[num_pos_obs]) * 1
+    sigma_model = integrate.simps(yy,xx)
+    sigma_random = integrate.simps(xx,xx)
+    
+    ar_value = (sigma_model - sigma_random) / (sigma_ideal - sigma_random)
+    
+    fig, ax = plt.subplots(nrows = 1, ncols = 1)
+    ax.plot(ideal['x'],ideal['y'], color='grey', label='Perfect Model')
+    ax.plot(xx,yy, color='red', label='User Model')
+    ax.plot(xx,xx, color='blue', label='Random Model')
+    ax.plot([percent, percent], [0.0, val], color='green', linestyle='--', linewidth=1)
+    ax.plot([0, percent], [val, val], color='green', linestyle='--', linewidth=1, label=str(val*100)+'% of positive obs at '+str(percent*100)+'%')
+    
+    plt.xlim(0, 1.02)
+    plt.ylim(0, 1.25)
+    plt.title("CAP Curve"+title+"- a_r value ="+str(ar_value))
+    plt.xlabel('% of the data')
+    plt.ylabel('% of positive obs')
+    plt.legend()
+#### CAP Analysis#####      
      
+capcurve(y_train,classifier.predict_proba(x_train)[:,1],"Train")
+capcurve(y_test,classifier.predict_proba(x_test)[:,1],"Test")
+      
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
 X_set, y_set = x_train, y_train
@@ -91,3 +144,6 @@ plt.title('Decision Tree Classifier - Entropy (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
+
+
+
