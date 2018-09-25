2cab460f117cbd0fc4fc3a367fd3719069fa2430
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-09-25 22:47:21 +0330

    Kernel PCA section added (non linear problem)

---
+++ Part 9 - Dimensionality Reduction/Section 25 - Kernel PCA/kernel_pca.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 22:36:20 2018
+
+@author: Mohammad Doosti Lakhani
+"""
+
+
+# Imporing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+x = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,-1].values
+
+# Convert categorical data with LabelEncoder
+from sklearn.preprocessing import LabelEncoder
+lbl_encoder = LabelEncoder()
+x[:,0] = lbl_encoder.fit_transform(x[:,0]) # convert gender to 0 and 1
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+standardscaler_x = StandardScaler()
+standardscaler_x = standardscaler_x.fit(x)
+x = standardscaler_x.transform(x)
+
+# Applying KernelPCA on x
+from sklearn.decomposition import KernelPCA
+pca = KernelPCA(n_components=2, kernel='rbf')
+x_pca =pca.fit_transform(x)
+
+# Splitting dataset into Train set and Test set
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x_pca,y, train_size = 0.8 , random_state=0)
+
+# Fitting Logistic Regression model to train data
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0, solver='liblinear')
+classifier = classifier.fit(x_train,y_train)
+
+# Make the prediction on train set
+y_train_pred = classifier.predict(x_train)
+
+# Make the prediction on train set
+y_test_pred = classifier.predict(x_test)
+
+# Acurracy on test and train set
+from sklearn.metrics import confusion_matrix
+cm_train = confusion_matrix(y_train,y_train_pred)
+cm_test = confusion_matrix(y_test,y_test_pred)
+
+import os
+import sys
+
+scriptpath = "../../Tools" # functions of acc and CAP
+# Add the directory containing your module to the Python path
+sys.path.append(os.path.abspath(scriptpath))
+import accuracy as ac
+
+t_train,f_train,acc_train = ac.accuracy_on_cm(cm_train)
+print('Train status = #{} True, #{} False, %{} Accuracy'.format(t_train,f_train,acc_train*100))
+
+t_test,f_test,acc_test = ac.accuracy_on_cm(cm_test)
+print('Test status = #{} True, #{} False, %{} Accuracy'.format(t_test,f_test,acc_test*100))
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green','gray')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green','gray'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green','gray')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green','gray'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.legend()
+plt.show()      
